e583d87a37232c9a62f6703598831f16ce768638
unknown <danovuksa3232@gmail.com>
2023-11-06 10:59:57 -0300

    deploy db change

--- api/src/app.js
+++ api/src/app.js
@@ -16,16 +16,16 @@ server.use(bodyParser.json({ limit: '50mb' }));
 server.use(cookieParser());
 server.use(morgan('dev'));
 server.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://videogamespi-gikh.onrender.com'); // update to match the domain you will make the request from
+  res.header('Access-Control-Allow-Origin', 'https://videogamespi-hdsp.onrender.com'); // update to match the domain you will make the request from
   res.header('Access-Control-Allow-Credentials', 'true');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
   next();
 });
-server.use(cors({
-  origin:'https://videogamespi-gikh.onrender.com',
-  // optionsSuccessStatus: 200
-}));
+// server.use(cors({
+//   origin:'https://videogamespi-gikh.onrender.com',
+//   // optionsSuccessStatus: 200
+// }));
 
 server.use('/', routes);
 
