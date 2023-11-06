5b98ba267a49f9bbe86b9532feb04c05df37774b
unknown <danovuksa3232@gmail.com>
2023-11-06 11:08:25 -0300

    deploy db change

--- api/src/app.js
+++ api/src/app.js
@@ -15,17 +15,20 @@ server.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
 server.use(bodyParser.json({ limit: '50mb' }));
 server.use(cookieParser());
 server.use(morgan('dev'));
-server.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://videogamespi-hsdp.onrender.com'); // update to match the domain you will make the request from
-  res.header('Access-Control-Allow-Credentials', 'true');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
-  next();
-});
-// server.use(cors({
-//   origin:'https://videogamespi-gikh.onrender.com',
-//   // optionsSuccessStatus: 200
-// }));
+// server.use((req, res, next) => {
+//   res.header('Access-Control-Allow-Origin', 'https://videogamespi-hsdp.onrender.com'); // update to match the domain you will make the request from
+//   res.header('Access-Control-Allow-Credentials', 'true');
+//   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+//   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+//   next();
+// });
+server.use(cors({
+  origin: 'https://videogamespi-hsdp.onrender.com',
+  credentials: true,
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  headers: 'Origin, X-Requested-With, Content-Type, Accept',
+}));
+
 
 server.use('/', routes);
 
